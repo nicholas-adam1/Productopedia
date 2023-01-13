d79b8a2a380ef076a56affe649be369378d3ddd5
Nick Adam <na4hx@virginia.edu>
2023-01-13 12:30:13 -0500

    Adds MVC folder structure

--- Productopedia/Controllers/SearchScreen.swift
+++ Productopedia/Controllers/SearchScreen.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Productopedia/Models/APIHandler.swift
@@ -0,0 +1,8 @@
+//
+//  APIHandler.swift
+//  Productopedia
+//
+//  Created by Nick Adam on 1/13/23.
+//
+
+import Foundation
